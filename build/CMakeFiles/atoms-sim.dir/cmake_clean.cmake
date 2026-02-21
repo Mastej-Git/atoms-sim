4001c8cc3a10eb9cbb45e36dcaa904be67129dab
Michal Mastej <m.mastej1812@gmail.com>
2026-02-21 20:32:14 +0100

    First commit - working window

---
+++ build/CMakeFiles/atoms-sim.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atoms-sim.dir/link.d"
+  "CMakeFiles/atoms-sim.dir/glad.c.o"
+  "CMakeFiles/atoms-sim.dir/glad.c.o.d"
+  "CMakeFiles/atoms-sim.dir/main.cpp.o"
+  "CMakeFiles/atoms-sim.dir/main.cpp.o.d"
+  "atoms-sim"
+  "atoms-sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/atoms-sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
