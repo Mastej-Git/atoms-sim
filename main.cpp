3a5d459f3c4b46ae3fda11dd71bc00e8d46983a3
Michal Mastej <m.mastej1812@gmail.com>
2026-02-23 23:53:14 +0100

    Refactored main to encapsulate Engine

--- main.cpp
+++ main.cpp
@@ -2,34 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main() {
+class Engine {
 
-    if (!glfwInit()) return -1;
+    GLFWwindow* window;
+    int width = 800;
+    int height = 600;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+public:
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
-    if (!window) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
+    Engine() {
+
+        if (!glfwInit()) {
+            std::cout << "Failed to initialize glfw" << std::endl;
+            exit(-1);
+        }
+
+        this->window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+        if (!window) {
+            std::cout << "Failed to create window" << std::endl;
+            glfwTerminate();
+            exit(-1);
+        }
+
+        glfwMakeContextCurrent(window);
+
+        if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            exit(-1);
+        }
+
+        int fb_width, fb_height;
+        glfwGetFramebufferSize(window, &fb_width, &fb_height);
+        glViewport(0, 0, fb_width, fb_height);
     }
-    glfwMakeContextCurrent(window);
 
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+    ~Engine() {
+        glfwTerminate();   
     }
 
-    glViewport(0, 0, 800, 600);
-
-    while (!glfwWindowShouldClose(window)) {
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
-
+    void run() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -37,6 +48,19 @@ int main() {
         glfwPollEvents();
     }
 
-    glfwTerminate();
+    GLFWwindow* get_window() {
+        return this->window;
+    }
+};
+
+int main() {
+
+    Engine engine;
+    // glfwSetKeyCallback(engine.window, key_callback)
+
+    while(!glfwWindowShouldClose(engine.get_window())) {
+        engine.run();
+    }
+
     return 0;
 }
